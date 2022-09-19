83d044f9a95282cf8d6370451e89a5fba3d1a53b
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-19 11:52:41 +0200

    Create blog post seeds

---
+++ seeds/blogpost-seeds.js
@@ -0,0 +1,18 @@
+const { BlogPost }  = require('../models');
+
+const postData = [
+    {
+        title: 'The Way We Were',
+        content: 'In the beginning was the void. Thereafter, there was some shit going down. Then we started fighting in the garden. Now we are all fucked.',
+        author_id: '1',
+    },
+    {
+        title: 'Papa Has A Brand New Bag',
+        content: 'Papa was a rolling stone. Where ever he laid his hat was his home. And when he died, all he left us was a new bag.',
+        author_id: '2',
+    }
+]
+
+const seedPosts = () => BlogPost.bulkCreate(postData);
+
+module.exports = seedPosts;
