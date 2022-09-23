6073cabf33e9846e45009a0c5913fcf249758566
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-23 11:13:47 +0200

    Create index.js for api route

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const blogPostRoutes = require('./blogPostRoutes');
+// router.use('/users', userRoutes);
+// router.use('/posts', blogPostRoutes);
+
+module.exports = router;
