1f1e311e592da319e7079ed0b14ad092aaaea643
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-25 15:59:14 +0200

    Remove blogPostRoutes until further notice

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const blogPostRoutes = require('./blogPostRoutes');
+// const blogPostRoutes = require('./blogPostRoutes');
 const userRoutes = require('./userRoutes');
 router.use('/user', userRoutes);
-router.use('/posts', blogPostRoutes);
+// router.use('/posts', blogPostRoutes);
 
 module.exports = router;
